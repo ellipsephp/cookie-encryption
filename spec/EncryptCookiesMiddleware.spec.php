e70f804261ff8e95b3643494c1680c28f114324d
pmall <pierre.mallinjoud@gmail.com>
2017-11-18 19:25:54 +0100

    add the middleware

---
+++ spec/EncryptCookiesMiddleware.spec.php
@@ -0,0 +1,247 @@
+<?php
+
+use function Eloquent\Phony\Kahlan\mock;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+use Interop\Http\Server\MiddlewareInterface;
+use Interop\Http\Server\RequestHandlerInterface;
+
+use Defuse\Crypto\Key;
+use Defuse\Crypto\Crypto;
+
+use Zend\Diactoros\Response\TextResponse;
+
+use Ellipse\Cookies\EncryptCookiesMiddleware;
+
+describe('EncryptCookiesMiddleware', function () {
+
+    beforeEach(function () {
+
+        $this->key = Key::createNewRandomKey();
+        $this->bypassed = ['bypassed'];
+
+        $this->middleware = new EncryptCookiesMiddleware($this->key, $this->bypassed);
+
+    });
+
+    it('should implement MiddlewareInterface', function () {
+
+        expect($this->middleware)->toBeAnInstanceOf(MiddlewareInterface::class);
+
+    });
+
+    describe('->process()', function () {
+
+        beforeEach(function () {
+
+            $this->request1 = mock(ServerRequestInterface::class);
+            $this->request2 = mock(ServerRequestInterface::class);
+            $this->response = new TextResponse('body', 404, ['header' => 'header-value']);
+
+            $this->handler = mock(RequestHandlerInterface::class);
+
+            $this->request1->getCookieParams->returns([]);
+
+            $this->request1->withCookieParams->returns($this->request2);
+
+            $this->handler->handle->returns($this->response);
+
+        });
+
+        it('should return a ResponseInterface', function () {
+
+            $test = $this->middleware->process($this->request1->get(), $this->handler->get());
+
+            expect($test)->toBeAnInstanceOf(ResponseInterface::class);
+
+        });
+
+        it('should call the request handler ->handle() method with the modified request', function () {
+
+            $this->middleware->process($this->request1->get(), $this->handler->get());
+
+            $this->handler->handle->calledWith($this->request2);
+
+        });
+
+        it('should return a response with the same body as the one returned by the request handler', function () {
+
+            $test = $this->middleware->process($this->request1->get(), $this->handler->get())
+                ->getBody()
+                ->getContents();
+
+            expect($test)->toEqual('body');
+
+        });
+
+        it('should return a response with the same status code as the one returned by the request handler', function () {
+
+            $test = $this->middleware->process($this->request1->get(), $this->handler->get())
+                ->getStatusCode();
+
+            expect($test)->toEqual(404);
+
+        });
+
+        it('should return a response with the same headers as the one returned by the request handler', function () {
+
+            $test = $this->middleware->process($this->request1->get(), $this->handler->get());
+
+            expect($test->getHeaderLine('Content-type'))->toContain('text');
+            expect($test->getHeaderLine('header'))->toContain('header-value');
+
+        });
+
+        context('when the request has a non bypassed cookie attached to it', function () {
+
+            context('when the encryption is valid', function () {
+
+                it('should attach a cookie with the same name and the decrypted value to the request', function () {
+
+                    $this->request1->getCookieParams->returns([
+                        'encrypted' => Crypto::encrypt('value', $this->key),
+                    ]);
+
+                    $this->middleware->process($this->request1->get(), $this->handler->get());
+
+                    $this->request1->withCookieParams->calledWith([
+                        'encrypted' => 'value',
+                    ]);
+
+                });
+
+            });
+
+            context('when the encryption is not valid', function () {
+
+                it('should attach a cookie with the same name and a blank string value to the request', function () {
+
+                    $this->request1->getCookieParams->returns([
+                        'encrypted' => 'value',
+                    ]);
+
+                    $this->middleware->process($this->request1->get(), $this->handler->get());
+
+                    $this->request1->withCookieParams->calledWith([
+                        'encrypted' => '',
+                    ]);
+
+                });
+
+            });
+
+        });
+
+        context('when the request has a bypassed cookie attached to it', function () {
+
+            it('should attach a cookie with the same name and value to the request', function () {
+
+                $this->request1->getCookieParams->returns([
+                    'bypassed' => 'value',
+                ]);
+
+                $this->middleware->process($this->request1->get(), $this->handler->get());
+
+                $this->request1->withCookieParams->calledWith([
+                    'bypassed' => 'value',
+                ]);
+
+            });
+
+        });
+
+        context('when the request has multple cookie attached to it', function () {
+
+            it('should attach them all to the new request', function () {
+
+                $this->request1->getCookieParams->returns([
+                    'encrypted' => Crypto::encrypt('encrypted', $this->key),
+                    'wrong' => 'wrong',
+                    'bypassed' => 'bypassed',
+                ]);
+
+                $this->middleware->process($this->request1->get(), $this->handler->get());
+
+                $this->request1->withCookieParams->calledWith([
+                    'encrypted' => 'encrypted',
+                    'wrong' => '',
+                    'bypassed' => 'bypassed',
+                ]);
+
+            });
+
+        });
+
+        context('when the response has a non bypassed cookie attached to it', function () {
+
+            it('should attach a cookie with the same name and the encrypted value to the response', function () {
+
+                $response = new TextResponse('body', 404, ['set-cookie' => 'encrypted=value']);
+
+                $this->handler->handle->returns($response);
+
+                $header = $this->middleware->process($this->request1->get(), $this->handler->get())
+                    ->getHeaderLine('set-cookie');
+
+                $parts = explode('=', $header);
+
+                $value = end($parts);
+
+                $test = Crypto::decrypt($value, $this->key);
+
+                expect($test)->toEqual('value');
+
+            });
+
+        });
+
+        context('when the response has a bypassed cookie attached to it', function () {
+
+            it('should attach a cookie with the same name and value to the response', function () {
+
+                $response = new TextResponse('body', 404, ['set-cookie' => 'bypassed=value']);
+
+                $this->handler->handle->returns($response);
+
+                $test = $this->middleware->process($this->request1->get(), $this->handler->get())
+                    ->getHeaderLine('set-cookie');
+
+                expect($test)->toContain('bypassed=value');
+
+            });
+
+        });
+
+        context('when the response has multple cookie attached to it', function () {
+
+            it('should attach them all to the response', function () {
+
+                $response = new TextResponse('body', 404, ['set-cookie' => ['encrypted=encrypted', 'bypassed=bypassed']]);
+
+                $this->handler->handle->returns($response);
+
+                $headers = $this->middleware->process($this->request1->get(), $this->handler->get())
+                    ->getHeader('set-cookie');
+
+                $parts1 = explode('=', $headers[0]);
+
+                $value = end($parts1);
+
+                $test1 = Crypto::decrypt($value, $this->key);
+
+                $parts2 = explode('=', $headers[1]);
+
+                $test2 = end($parts2);
+
+                expect($test1)->toEqual('encrypted');
+                expect($test2)->toEqual('bypassed');
+
+            });
+
+        });
+
+    });
+
+});
---
+++ src/EncryptCookiesMiddleware.php
@@ -0,0 +1,153 @@
+<?php declare(strict_types=1);
+
+namespace Ellipse\Cookies;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+use Interop\Http\Server\MiddlewareInterface;
+use Interop\Http\Server\RequestHandlerInterface;
+
+use Dflydev\FigCookies\SetCookie;
+use Dflydev\FigCookies\SetCookies;
+use Dflydev\FigCookies\FigResponseCookies;
+
+use Defuse\Crypto\Key;
+use Defuse\Crypto\Crypto;
+use Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException;
+
+class EncryptCookiesMiddleware implements MiddlewareInterface
+{
+    /**
+     * The defuse encryption keu.
+     *
+     * @var \Defuse\Crypto\Key
+     */
+    private $key;
+
+    /**
+     * The names of the cookies which bypass encryption.
+     *
+     * @var array
+     */
+    private $bypassed;
+
+    /**
+     * Set up a encrypt cookie middleware with the given defuse key and an array
+     * of bypassed cookie names.
+     *
+     * @param \Defuse\Crypto\Key    $key
+     * @param array                 $bypassed
+     */
+    public function __construct(Key $key, array $bypassed = [])
+    {
+        $this->key = $key;
+        $this->bypassed = $bypassed;
+    }
+
+    /**
+     * Start the session, delegate the request processing and add the session
+     * cookie to the response.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface      $request
+     * @param \Interop\Http\Server\RequestHandlerInterface  $handler
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $request = $this->withDecryptedCookies($request);
+
+        $response = $handler->handle($request);
+
+        return $this->withEncryptedCookies($response);
+    }
+
+    /**
+     * Encrypt the given value using the key.
+     *
+     * @param string $value
+     * @return string
+     */
+    private function encrypt(string $value): string
+    {
+        return Crypto::encrypt($value, $this->key);
+    }
+
+    /**
+     * Decrypt the given value using the key. Default to blank string when the
+     * key is wrong or the cypher text has been modified.
+     *
+     * @param string $value
+     * @return string
+     */
+    private function decrypt(string $value): string
+    {
+        try {
+
+            return Crypto::decrypt($value, $this->key);
+
+        }
+
+        catch (WrongKeyOrModifiedCiphertextException $e) {
+
+            return '';
+
+        }
+    }
+
+    /**
+     * Decrypt the non bypassed cookie values attached to the given request
+     * and return a new request with those values.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    private function withDecryptedCookies(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $cookies = $request->getCookieParams();
+
+        $decrypted = [];
+
+        foreach ($cookies as $name => $value) {
+
+            $decrypted[$name] = in_array($name, $this->bypassed)
+                ? $value
+                : $this->decrypt($value);
+
+        }
+
+        return $request->withCookieParams($decrypted);
+    }
+
+    /**
+     * Encrypt the non bypassed cookie values attached to the given response
+     * and return a new response with those values.
+     *
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    private function withEncryptedCookies(ResponseInterface $response): ResponseInterface
+    {
+        $cookies = (SetCookies::fromResponse($response))->getAll();
+
+        foreach ($cookies as $cookie) {
+
+             $name = $cookie->getName();
+
+             if (in_array($name, $this->bypassed)) continue;
+
+             $response = FigResponseCookies::modify($response, $name, function (SetCookie $cookie) {
+
+                 $value = $cookie->getValue();
+
+                 $encrypted = $this->encrypt($value);
+
+                 return $cookie->withValue($encrypted);
+
+             });
+
+         }
+
+         return $response;
+    }
+}
